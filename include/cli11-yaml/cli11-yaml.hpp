1a79b90faa56cb2f9e9b13362df4a9e20ae445a2
David Fleury <David.Fleury@viavisolutions.com>
2022-09-23 23:35:37 +0200

    Start full parse rewrite

--- include/cli11-yaml/cli11-yaml.hpp
+++ include/cli11-yaml/cli11-yaml.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <CLI/Config.hpp>
-#include <yaml-cpp/yaml.h>
 
 namespace CLI {
 
@@ -9,16 +8,13 @@ namespace CLI {
 class ConfigYAML : public Config {
 public:
     /// Convert an app into a configuration
-    std::string to_config(const App *, bool, bool, std::string) const override;
+    std::string to_config(const App *, bool, bool, std::string) const override
+    {
+        return {};
+    }
 
     /// Convert a configuration into an app
     std::vector<ConfigItem> from_config(std::istream& is) const override;
-
-private:
-    static std::string indent(unsigned level) { return std::string(level * 2, ' '); }
-
-    std::vector<ConfigItem> parse(const YAML::Node& node, std::vector<std::string> parents, unsigned level = 0) const;
-    static void aggregate(std::vector<ConfigItem>&);
 };
 
 }
--- src/cli11-yaml.cpp
+++ src/cli11-yaml.cpp
@@ -1,47 +1,16 @@
 #include <cli11-yaml/cli11-yaml.hpp>
 
+#include <yaml-cpp/yaml.h>
+
 namespace CLI {
 
-inline std::string
-ConfigYAML::to_config(const App *, bool, bool, std::string) const {
-    return {};
-}
-
-inline std::vector<ConfigItem>
-ConfigYAML::from_config(std::istream& is) const {
-    YAML::Node config = YAML::Load(is);
-    auto tmp_items = parse(config, {});
-    aggregate(tmp_items);
-    return tmp_items;
-}
-
-inline void
-ConfigYAML::aggregate(std::vector<ConfigItem>& items)
+std::vector<ConfigItem>
+ConfigYAML::from_config(std::istream& is) const
 {
-    // Aggregate identical config items (parents/name) in inputs
-    if(items.size() > 1) {
-        for(auto it = items.begin(); it != items.end(); ++it) {
-            for(auto jt = it + 1; jt != items.end();) {
-                auto& lh = *it;
-                auto& rh = *jt;
-                if (lh.parents == rh.parents && lh.name == rh.name) {
-                    for(auto v : rh.inputs) {
-                        lh.inputs.push_back(v);
-                    }
-                    jt = items.erase(jt);
-                }
-                else {
-                    ++jt;
-                }
-            }
-        }
-    }
-}
-
-inline std::vector<ConfigItem>
-ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsigned level) const {
+    YAML::Node node = YAML::Load(is);
 
     std::vector<ConfigItem> output;
+    std::vector<std::string> parents;
 
 //    std::cout << "Create Parent: " << detail::join(parents) << std::endl;
     switch (node.Type()) {
@@ -49,9 +18,9 @@ ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsi
 //        std::cout << indent(level) << "Null" << std::endl;
 
             ConfigItem config_item;
-            config_item.name = (parents.empty() ? "": *parents.rbegin());
+            config_item.name = (parents.empty() ? "" : *parents.rbegin());
             config_item.parents = parents;
-            if(!config_item.parents.empty())
+            if (!config_item.parents.empty())
                 config_item.parents.erase(config_item.parents.rbegin().base());
             config_item.inputs.emplace_back("true");
             output.push_back(std::move(config_item));
@@ -69,18 +38,19 @@ ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsi
             config_item.name = parents.empty() ? "" : *parents.rbegin();
 
             config_item.parents = parents;
-            if(!config_item.parents.empty())
+            if (!config_item.parents.empty())
                 config_item.parents.erase(config_item.parents.rbegin().base());
 
-            for(YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
-                if(it->IsScalar()) {
+            for (YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
+                if (it->IsScalar()) {
 //                std::cout << indent(level + 1) << "Value: " << it->as<std::string>() << '\n';
 
                     config_item.inputs.push_back(it->as<std::string>());
-                } else {
-                    for(auto ci : parse(*it, parents, level + 1)) {
-                        output.push_back(std::move(ci));
-                    }
+                }
+                else {
+//                    for (auto ci: parse(*it, parents, level + 1)) {
+//                        output.push_back(std::move(ci));
+//                    }
                 }
             }
             output.push_back(std::move(config_item));
@@ -89,9 +59,9 @@ ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsi
         case YAML::NodeType::Map: {
 //        std::cout << indent(level) << "Map: " << std::endl;
 
-            for(YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
+            for (YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
 //            std::cout << indent(level) << "Key: " << it->first.as<std::string>();
-                if(it->second.IsScalar()) {
+                if (it->second.IsScalar()) {
 //                std::cout << ": Value: " << it->second.as<std::string>() << '\n';
 
                     ConfigItem config_item;
@@ -100,13 +70,14 @@ ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsi
                     config_item.inputs.push_back(it->second.as<std::string>());
                     output.push_back(std::move(config_item));
 
-                } else {
+                }
+                else {
 //                std::cout << ":\n";
                     auto tmp = parents;
                     tmp.push_back(it->first.as<std::string>());
-                    for(auto ci : parse(it->second, tmp, level + 1)) {
-                        output.push_back(std::move(ci));
-                    }
+//                    for (auto ci: parse(it->second, tmp, level + 1)) {
+//                        output.push_back(std::move(ci));
+//                    }
                 }
             }
             break;
