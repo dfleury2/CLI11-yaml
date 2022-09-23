b600b465e567599734becb77f4e5069cd4d58c17
David Fleury <David.Fleury@viavisolutions.com>
2022-09-23 23:44:32 +0200

    Revert "Auxiliary commit to revert individual files from 1a79b90faa56cb2f9e9b13362df4a9e20ae445a2"
    This reverts commit f9e19b694353f3a8e9b59a5bf2fb9e38ba8459b2.

--- include/cli11-yaml/cli11-yaml.hpp
+++ include/cli11-yaml/cli11-yaml.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <CLI/Config.hpp>
+#include <yaml-cpp/yaml.h>
 
 namespace CLI {
 
@@ -8,13 +9,16 @@ namespace CLI {
 class ConfigYAML : public Config {
 public:
     /// Convert an app into a configuration
-    std::string to_config(const App *, bool, bool, std::string) const override
-    {
-        return {};
-    }
+    std::string to_config(const App *, bool, bool, std::string) const override;
 
     /// Convert a configuration into an app
     std::vector<ConfigItem> from_config(std::istream& is) const override;
+
+private:
+    static std::string indent(unsigned level) { return std::string(level * 2, ' '); }
+
+    std::vector<ConfigItem> parse(const YAML::Node& node, std::vector<std::string> parents, unsigned level = 0) const;
+    static void aggregate(std::vector<ConfigItem>&);
 };
 
 }
--- src/cli11-yaml.cpp
+++ src/cli11-yaml.cpp
@@ -1,16 +1,47 @@
 #include <cli11-yaml/cli11-yaml.hpp>
 
-#include <yaml-cpp/yaml.h>
-
 namespace CLI {
 
-std::vector<ConfigItem>
-ConfigYAML::from_config(std::istream& is) const
+inline std::string
+ConfigYAML::to_config(const App *, bool, bool, std::string) const {
+    return {};
+}
+
+inline std::vector<ConfigItem>
+ConfigYAML::from_config(std::istream& is) const {
+    YAML::Node config = YAML::Load(is);
+    auto tmp_items = parse(config, {});
+    aggregate(tmp_items);
+    return tmp_items;
+}
+
+inline void
+ConfigYAML::aggregate(std::vector<ConfigItem>& items)
 {
-    YAML::Node node = YAML::Load(is);
+    // Aggregate identical config items (parents/name) in inputs
+    if(items.size() > 1) {
+        for(auto it = items.begin(); it != items.end(); ++it) {
+            for(auto jt = it + 1; jt != items.end();) {
+                auto& lh = *it;
+                auto& rh = *jt;
+                if (lh.parents == rh.parents && lh.name == rh.name) {
+                    for(auto v : rh.inputs) {
+                        lh.inputs.push_back(v);
+                    }
+                    jt = items.erase(jt);
+                }
+                else {
+                    ++jt;
+                }
+            }
+        }
+    }
+}
+
+inline std::vector<ConfigItem>
+ConfigYAML::parse(const YAML::Node& node, std::vector<std::string> parents, unsigned level) const {
 
     std::vector<ConfigItem> output;
-    std::vector<std::string> parents;
 
 //    std::cout << "Create Parent: " << detail::join(parents) << std::endl;
     switch (node.Type()) {
@@ -18,9 +49,9 @@ ConfigYAML::from_config(std::istream& is) const
 //        std::cout << indent(level) << "Null" << std::endl;
 
             ConfigItem config_item;
-            config_item.name = (parents.empty() ? "" : *parents.rbegin());
+            config_item.name = (parents.empty() ? "": *parents.rbegin());
             config_item.parents = parents;
-            if (!config_item.parents.empty())
+            if(!config_item.parents.empty())
                 config_item.parents.erase(config_item.parents.rbegin().base());
             config_item.inputs.emplace_back("true");
             output.push_back(std::move(config_item));
@@ -38,19 +69,18 @@ ConfigYAML::from_config(std::istream& is) const
             config_item.name = parents.empty() ? "" : *parents.rbegin();
 
             config_item.parents = parents;
-            if (!config_item.parents.empty())
+            if(!config_item.parents.empty())
                 config_item.parents.erase(config_item.parents.rbegin().base());
 
-            for (YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
-                if (it->IsScalar()) {
+            for(YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
+                if(it->IsScalar()) {
 //                std::cout << indent(level + 1) << "Value: " << it->as<std::string>() << '\n';
 
                     config_item.inputs.push_back(it->as<std::string>());
-                }
-                else {
-//                    for (auto ci: parse(*it, parents, level + 1)) {
-//                        output.push_back(std::move(ci));
-//                    }
+                } else {
+                    for(auto ci : parse(*it, parents, level + 1)) {
+                        output.push_back(std::move(ci));
+                    }
                 }
             }
             output.push_back(std::move(config_item));
@@ -59,9 +89,9 @@ ConfigYAML::from_config(std::istream& is) const
         case YAML::NodeType::Map: {
 //        std::cout << indent(level) << "Map: " << std::endl;
 
-            for (YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
+            for(YAML::const_iterator it = node.begin(); it != node.end(); ++it) {
 //            std::cout << indent(level) << "Key: " << it->first.as<std::string>();
-                if (it->second.IsScalar()) {
+                if(it->second.IsScalar()) {
 //                std::cout << ": Value: " << it->second.as<std::string>() << '\n';
 
                     ConfigItem config_item;
@@ -70,14 +100,13 @@ ConfigYAML::from_config(std::istream& is) const
                     config_item.inputs.push_back(it->second.as<std::string>());
                     output.push_back(std::move(config_item));
 
-                }
-                else {
+                } else {
 //                std::cout << ":\n";
                     auto tmp = parents;
                     tmp.push_back(it->first.as<std::string>());
-//                    for (auto ci: parse(it->second, tmp, level + 1)) {
-//                        output.push_back(std::move(ci));
-//                    }
+                    for(auto ci : parse(it->second, tmp, level + 1)) {
+                        output.push_back(std::move(ci));
+                    }
                 }
             }
             break;
